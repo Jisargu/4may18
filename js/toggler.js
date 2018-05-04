68e62cad029cbc187aef6a50f50b945e90e16dbe
Jisargu <jonathnsaa@gmail.com>
2018-05-04 15:08:08 -0500

    toggler js.  Usar un boton con una funcion toggler para agregar o quitar  una clase

---
+++ js/toggler.js
@@ -0,0 +1,5 @@
+function desaparece(){
+    var elemento=document.getElementById("barrita");
+    elemento.classList.toggle("desaparece");
+    //Es lo mismo que document.getElementById("barrita").classList.toggle("desaparece");
+}
